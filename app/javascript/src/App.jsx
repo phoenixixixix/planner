1d876c0d565f478d9aeb724ed7217d746a800c54
phoenixixixix <barchukoleg12@gmail.com>
2023-05-07 22:31:29 +0300

    Added ability to display list of tasks

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -2,27 +2,34 @@ import React, { useEffect, useState } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
+import Dashboard from "components/Dashboard";
+import PageLoader from "components/PageLoader";
 
 const App = () => {
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        initializeLogger();
-        setAuthHeaders(setLoading);
-    }, []);
+  useEffect(() => {
+    initializeLogger();
+    setAuthHeaders(setLoading);
+  }, []);
 
-    if (loading) {
-        return <h1>Loading...</h1>;
-    }
-
+  if (loading) {
     return (
-        <Router>
-            <Switch>
-                <Route exact path="/" render={() => <div>Home</div>} />
-                <Route exact path="/about" render={() => <div>About</div>} />
-            </Switch>
-        </Router>
+      <div className="h-screen">
+          <PageLoader />
+      </div>
     );
+  }
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <div>Home</div>} />
+        <Route exact path="/about" render={() => <div>About</div>} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
