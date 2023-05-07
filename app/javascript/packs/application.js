9c1bb5c9a1b2826b127236dc4053cec5aec1e023
phoenixixixix <barchukoleg12@gmail.com>
2023-05-07 14:19:12 +0300

    Added React.js to the project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,12 @@
 // const imagePath = (name) => images(name, true)
 
 import "../stylesheets/application.scss"
+
+// Support component names relative to this directory:
+var componentRequireContext = require.context("src", true);
+var ReactRailsUJS = require("react_ujs");
+
+// Makes React dev tools etc, work.
+ReactRailsUJS.mountComponents();
+
+ReactRailsUJS.useContext(componentRequireContext);
